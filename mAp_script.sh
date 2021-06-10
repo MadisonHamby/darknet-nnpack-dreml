6c246f3dc7d8b3f8bd3111ac42c9febdcc30740a
Madison Hamby <mahamby99@gmail.com>
2021-06-10 15:31:21 -0500

    Created bash script to run darknet multiple times and save results.

---
+++ mAp_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run a certain amount of frames
+# Saves frames as frame#.txt
+#make
+
+for i in {25000..25050}
+do
+    # Run darknet
+    ./darknet detect cfg/yolov3.cfg yolov3.weights data/ZurichMAV/Frames/$i.jpg
+    #echo "$i\n"
+
+    # Save det-results.txt as 25000.txt in data/ZurichMAV/DetectionResults
+    cp det_result.txt data/ZurichMAV/DetectionResults/$i.txt
+done
