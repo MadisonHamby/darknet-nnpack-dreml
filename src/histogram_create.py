b6e247732ff95cc25bb517f6aa334cf4749eb2b3
Madison Hamby <mahamby99@gmail.com>
2021-06-16 10:55:37 -0500

    Created python script to graph histogram of tensor values

--- src/histogram_create.py
+++ src/histogram_create.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv('/home/osboxes/darknet-nnpack-dreml/output.csv')
+df.head(1)
 
+df[' Tensor Value'].plot(kind='hist')
+plt.title('Tensor Values for Figure #25000')
+plt.xlabel('Tensor Value')
+plt.show()
+#print(df[' Tensor Value'])
