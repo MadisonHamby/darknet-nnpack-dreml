974738721f7871850453057dd02efe046c028297
Madison Hamby <mahamby99@gmail.com>
2021-06-22 13:38:33 -0500

    Fixed bug by adding in the bias

--- scripts/ieee_to_custom.c
+++ scripts/ieee_to_custom.c
@@ -94,34 +94,34 @@ int main()
 
     // cut down to:
     // 0 | 1000 | 100
-
     int signed_bit = var.raw.sign; // get signed bit
     // signed bit will be 1000 0000
     // or 0000 0000
+    //signed_bit = signed_bit << 8;
     printf("Signed bit: ");
-    printBinary(signed_bit, 8);
+    printBinary(signed_bit, 1);
     printf("\n");
 
-    int exp_bits = var.raw.exponent >> 1;
-    // exp_bits ex: if exponent bits are 111
-    // Store like 1110 0000 (8 bit number)
-    // Right shift over 1 to leave room for signed bit
-    // exp_bits = 0111 0000
-    printf("Exponent bits: ");
-    printBinary(exp_bits, 8);
+    int exp_bits;
+    printf("The original exponent with bias is: %d\n", var.raw.exponent);
+    exp_bits = abs(var.raw.exponent - 127);
+    printf("Exponent without bias is: %d\n", exp_bits);
+    // our bias for 8 bit is 8
+    printf("Exponent bits without bias: ");
+    printBinary(exp_bits, 4);
     printf("\n");
 
     int mantissa_bits = var.raw.mantissa >> (23 - 3);
     // get mantissa bits (shift over 23 - # of mantissa bits)
     printf("Mantissa bits: ");
-    printBinary(mantissa_bits, 8);
+    printBinary(mantissa_bits, 3);
     printf("\n");
 
     int final_8_bit = 0; // where we will store final 8 bit
-    final_8_bit = final_8_bit | signed_bit; // OR with 0
+    final_8_bit = final_8_bit | signed_bit << 7; // OR with 0
     //printBinary(final_8_bit, 8);
     //printf("\n");
-    final_8_bit = final_8_bit | exp_bits;
+    final_8_bit = final_8_bit | exp_bits << 3;
     //printBinary(final_8_bit, 8);
     //printf("\n");
     final_8_bit = final_8_bit | mantissa_bits;
@@ -133,7 +133,9 @@ int main()
     myfloat ieee;
     ieee.f = 0;
     ieee.raw.sign = signed_bit;
-    ieee.raw.exponent = exp_bits << 1;
+    // add back in the bias
+    exp_bits = exp_bits + 127;
+    ieee.raw.exponent = exp_bits;
     ieee.raw.mantissa = mantissa_bits << (23 - 3);
     printf("Back to IEEE: %lf\n", ieee.f);
 
